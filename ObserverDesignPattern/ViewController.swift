6a29e110e376b0106e0696f6ad09737784063ded
Sivaram Yadav <nsr.learnings@gmail.com>
2022-11-02 13:17:18 +0530

    Added a sample code for observer pattern

--- ObserverDesignPattern/ViewController.swift
+++ ObserverDesignPattern/ViewController.swift
@@ -13,7 +13,74 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
+
+// points in observer pattern.
+// 1. Pub-Sub model
+// 2. Sub must follow some common methods for that we use one protocol
+//'3. Pub has array of observables references.
+// 4. Pub will notify the observabers by calling their methods.
 
 
+protocol DownloadManagerDelegate {
+    func urlSessionNew(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?)
+}
+
+protocol Observable {
+    func didReceiveError(error: Error)
+    func update()
+}
+
+enum DownloadState {
+    case downloadComplete
+    case downloadError(Error)
+}
+
+
+class DownloadManager: DownloadManagerDelegate {
+    
+    static let shared: DownloadManager = DownloadManager()
+    
+    private var observers: [Observable] = []
+    
+    func addObserver(observer: Observable) {
+        observers.append(observer)
+    }
+    
+    func removeObserver(observer: Observable) {
+        // Logic to remove observer
+    }
+    
+    func notifyObservers(downloadState: DownloadState) {
+        
+        for observer in observers {
+            switch downloadState {
+            case .downloadComplete:
+                observer.update()
+            case .downloadError(let error):
+                observer.didReceiveError(error: error)
+            }
+        }
+    }
+    
+    func urlSessionNew(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        notifyObservers(downloadState: .downloadError(error!))
+    }
+}
+
+
+class MyDownloads: Observable {
+    
+    init() {
+        DownloadManager.shared.addObserver(observer: self)
+    }
+    
+    func update() {
+        
+    }
+    
+    func didReceiveError(error: Error) {
+        print("Eror:", error.localizedDescription)
+    }
 }
 
